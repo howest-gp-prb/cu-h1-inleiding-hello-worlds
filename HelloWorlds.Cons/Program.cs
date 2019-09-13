02e9a45e41d35b14b4efba02a51b7035e998cd1c
Dries Deboosere <dries.deboosere@howest.be>
2019-09-13 13:18:32 +0200

    Add project files.

---
+++ HelloWorlds.Cons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorlds.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* let's output some text 
+             * in the console window */
+            Console.WriteLine("Hello World");
+            string input = Console.ReadLine(); //wait for user to hit enter and read all text input
+        }
+    }
+}
